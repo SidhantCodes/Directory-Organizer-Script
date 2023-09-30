1d48513aa859a5d05b2390891bdd51bbffa2c0b8
Sidhant Mishra <mishrasidhant01@gmail.com>
2023-09-30 22:46:36 +0530

    Added feature so that the script will keep sorting the directory specified every 10mins

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import os
 import shutil
+import time
 
-targetFolder = 'insert url'
 
-ext = {item.split('.')[-1] for item in os.listdir(targetFolder) if os.path.isfile(os.path.join(targetFolder, item))}
 
-for extension in ext:
-    if not os.path.exists(os.path.join(targetFolder, extension)):
-        os.mkdir(os.path.join(targetFolder, extension))
-        
-        
-for item in os.listdir(targetFolder):
-    if os.path.isfile(os.path.join(targetFolder, item)):
-        fileExt = item.split('.')[-1]
-        
-        shutil.move(os.path.join(targetFolder, item), os.path.join(targetFolder, fileExt, item))
-        
+
+def sortDir(targetFolder):
+    ext = {item.split('.')[-1] for item in os.listdir(targetFolder) if os.path.isfile(os.path.join(targetFolder, item))}
+
+    for extension in ext:
+        if not os.path.exists(os.path.join(targetFolder, extension)):
+            os.mkdir(os.path.join(targetFolder, extension))
+            
+            
+    for item in os.listdir(targetFolder):
+        if os.path.isfile(os.path.join(targetFolder, item)):
+            fileExt = item.split('.')[-1]
+            
+            shutil.move(os.path.join(targetFolder, item), os.path.join(targetFolder, fileExt, item))
+            
+if __name__ == '__main__':
+    targetFolder = 'Direcrtory URL'
+    breakTime_mins = 10
+    
+    while True:
+        sortDir(targetFolder)
+        time.sleep(breakTime_mins * 60);
