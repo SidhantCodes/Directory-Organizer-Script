a1546843797de5c05f411d2b61a8ae9f07ab40fb
Sidhant Mishra <mishrasidhant01@gmail.com>
2023-09-30 22:21:23 +0530

    Completed Script

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ ext = {item.split('.')[-1] for item in os.listdir(targetFolder) if os.path.isfil
 for extension in ext:
     if not os.path.exists(os.path.join(targetFolder, extension)):
         os.mkdir(os.path.join(targetFolder, extension))
+        
+        
+for item in os.listdir(targetFolder):
+    if os.path.isfile(os.path.join(targetFolder, item)):
+        fileExt = item.split('.')[-1]
+        
+        shutil.move(os.path.join(targetFolder, item), os.path.join(targetFolder, fileExt, item))
+        
