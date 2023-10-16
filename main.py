f6004dc213818f8839842c448c0caa6369103f66
Sidhant Mishra <mishrasidhant01@gmail.com>
2023-10-17 01:30:08 +0530

    Fixed permission denied for dekstop.ini

--- main.py
+++ main.py
@@ -9,17 +9,26 @@ def sortDir(targetFolder):
         if not os.path.exists(os.path.join(targetFolder, extension)):
             os.mkdir(os.path.join(targetFolder, extension))
             
-            
     for item in os.listdir(targetFolder):
         if os.path.isfile(os.path.join(targetFolder, item)):
             fileExt = item.split('.')[-1]
+
+            if item.lower() == 'desktop.ini':
+                continue
             
-            shutil.move(os.path.join(targetFolder, item), os.path.join(targetFolder, fileExt, item))
+            src = os.path.join(targetFolder, item)
+            dst = os.path.join(targetFolder, fileExt, item)
             
+            try:
+                shutil.move(src, dst)
+            except PermissionError as e:
+                print(f"Error moving {src} to {dst}: {e}")
+                continue
+
 if __name__ == '__main__':
-    targetFolder = 'URL/To/Your/Target/Folder/Here'
+    targetFolder = 'URL/TO/TARGET/FOLDER'
     breakTime_mins = 10
     
     while True:
         sortDir(targetFolder)
-        time.sleep(breakTime_mins * 60);
+        time.sleep(breakTime_mins * 60)
